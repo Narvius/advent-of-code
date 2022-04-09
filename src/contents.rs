18f3e734c7f15ce63e8de0c384036f04cd0320ed
Phil <narvius@gmail.com>
2022-04-09 11:49:06 +0200

    day 2015-06

--- src/contents.rs
+++ src/contents.rs
@@ -30,7 +30,7 @@ static AOC2015: Section = [
     day!(aoc2015::day03),
     day!(aoc2015::day04),
     day!(aoc2015::day05),
-    None,
+    day!(aoc2015::day06),
     None,
     None,
     None,
---
+++ src/solutions/aoc2015/day06.rs
@@ -0,0 +1,56 @@
+/// Counts how many lights are on after all instructions are executed.
+pub fn one(input: &str) -> Result<String, String> {
+    Ok(run_lights(input, |b| if b { 1 } else { 0 }, |b, c| match c {
+        "toggle" => !b,
+        "on" => true,
+        "off" => false,
+        _ => unreachable!(),
+    }).to_string())
+}
+
+/// Counts the total brightness after all instructions are executed.
+pub fn two(input: &str) -> Result<String, String> {
+    Ok(run_lights(input, |b| b, |b, c| match c {
+        "toggle" => b + 2,
+        "on" => b + 1,
+        "off" => b.saturating_sub(1),
+        _ => unreachable!(),
+    }).to_string())
+}
+
+/// Executes the shared puzzle logic. For each line of inputs, runs `step` on all relevant
+/// coordinates, and at the end sums the outputs of `collect` for each position.
+fn run_lights<E>(input: &str, collect: fn(E) -> usize, step: fn(E, &str) -> E) -> usize
+where
+    E: Copy + Default,
+{
+    let mut lights = vec![E::default(); 1_000_000];
+    for (c, (x0, y0), (x1, y1)) in input.lines().filter_map(parse) {
+        for y in y0..=y1 {
+            for x in x0..=x1 {
+                let val = &mut lights[1000 * y + x];
+                *val = step(*val, c);
+            }
+        }
+    }
+    lights.into_iter().map(collect).sum::<usize>()
+}
+
+type Line<'a> = (&'a str, (usize, usize), (usize, usize));
+
+/// Parses a line of puzzle input.
+fn parse(line: &str) -> Option<Line> {
+    fn make<'a>(command: &'a str, a: &str, b: &str) -> Option<Line<'a>> {
+        let (x0, y0) = a.split_once(',')?;
+        let (x1, y1) = b.split_once(',')?;
+
+        Some((command, (x0.parse().ok()?, y0.parse().ok()?), (x1.parse().ok()?, y1.parse().ok()?)))
+    }
+
+    let tokens: Vec<_> = line.split(' ').collect();
+    match tokens.len() {
+        4 => make(tokens[0], tokens[1], tokens[3]),
+        5 => make(tokens[1], tokens[2], tokens[4]),
+        _ => None,
+    }
+}

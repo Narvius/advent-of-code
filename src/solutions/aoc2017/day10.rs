f9bf63cf088c6f926ccced4e0cf701b10622c91d
Phil <narvius@gmail.com>
2022-05-31 21:37:12 +0200

    2017-10 factor out knot_hash for future reuse

--- src/solutions/aoc2017/day10.rs
+++ src/solutions/aoc2017/day10.rs
@@ -12,15 +12,21 @@ pub fn one(input: &str) -> Result<String, String> {
 /// find the "dense hash", and write it as a hexadecimal string. Also uses an alternate
 /// interpretation of the puzzle input.
 pub fn two(input: &str) -> Result<String, String> {
-    let mut hasher = Hasher::new(input.bytes().chain([17, 31, 73, 47, 23]));
-    for _ in 0..64 {
-        hasher.run_once();
-    }
+    let input = input.bytes().chain([17, 31, 73, 47, 23]);
     let mut s = String::with_capacity(32);
-    for byte in hasher.dense_hash() {
+    for byte in knot_hash(input) {
         write!(s, "{:02x}", byte).expect("failed to write to preallocated string");
     }
     Ok(s)
+}
+
+/// Calculates the knot hash for a given sequence of bytes.
+pub fn knot_hash(bytes: impl IntoIterator<Item = u8>) -> Vec<u8> {
+    let mut hasher = Hasher::new(bytes);
+    for _ in 0..64 {
+        hasher.run_once();
+    }
+    hasher.dense_hash().collect()
 }
 
 /// Holds the state for knot hashing.

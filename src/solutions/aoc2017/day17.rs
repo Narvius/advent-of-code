85a957376bb75c3503e27a3043a12a44d247f6be
Phil <narvius@gmail.com>
2022-06-02 14:03:46 +0200

    2017-17 mildly refactor two

--- src/solutions/aoc2017/day17.rs
+++ src/solutions/aoc2017/day17.rs
@@ -14,18 +14,16 @@ pub fn one(input: &str) -> Result<String, String> {
 /// Find the element after 0 in a huge spinlock list.
 /// 
 /// Unlike [`one`], this does not require us to actually construct the list. We only need to
-/// keep track of which element was inserted at position 1 last, since 0 will forever remain
-/// the leading element.
+/// keep track of which element was inserted at position 0 last, since with the way we
+/// calculate insert positions, 0 will always be at the end of the list (and since the list
+/// is circular, the element at *index* 0 is the one after the *value* 0).
 pub fn two(input: &str) -> Result<String, String> {
     let skip: usize = input.parse().map_err(|_| "failed parse".to_owned())?;
     let mut last = 0;
     let mut position = 0;
     for i in 1..50000000 + 1 {
-        // Position formula differs slightly from the equivalent in `one`, because there we
-        // cared about being a valid index (and it wouldn't be on the final iteration), here we
-        // care about maintaining zero as lead.
-        position = (position + skip) % i + 1;
-        if position == 1 {
+        position = (position + skip + 1) % i;
+        if position == 0 {
             last = i;
         }
     }

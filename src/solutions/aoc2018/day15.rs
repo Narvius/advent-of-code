51b6ea7f094c925666bed91c077e4e5037e123c3
Phil <narvius@gmail.com>
2022-11-03 15:12:19 +0100

    2018-15 refactor

--- src/solutions/aoc2018/day15.rs
+++ src/solutions/aoc2018/day15.rs
@@ -2,13 +2,14 @@ use std::collections::HashSet;
 
 /// Run the battle, get the outcome.
 pub fn one(input: &str) -> crate::Result<i32> {
-    Ok(Battle::from_input(input, 3).run_to_completion().0)
+    Ok(Battle::from_input(input).run_to_completion().0)
 }
 
 /// Buff elves so they barely win without casualties; run the battle, get the outcome.
 pub fn two(input: &str) -> crate::Result<i32> {
+    let battle = Battle::from_input(input);
     for i in 4.. {
-        if let (outcome, true) = Battle::from_input(input, i).run_to_completion() {
+        if let (outcome, true) = battle.with_elf_power(i).run_to_completion() {
             return Ok(outcome);
         }
     }
@@ -16,7 +17,7 @@ pub fn two(input: &str) -> crate::Result<i32> {
     Err("unreachable".into())
 }
 
-/// A battle.
+/// A battle. Can be indexed with a `(i32, i32)` to get whether the selected tile is open.
 #[derive(Clone, Debug, Eq, PartialEq)]
 struct Battle {
     /// All units on the battlefield.
@@ -25,13 +26,15 @@ struct Battle {
     map: Vec<Vec<bool>>,
     /// The number of elves (initially).
     elf_count: usize,
+    /// Combat power of elves.
+    elf_power: i32,
     /// The amount of full turns that elapsed.
     turns: usize,
 }
 
 impl Battle {
     /// Parses the puzzle input into a ready-to-run battle.
-    fn from_input(input: &str, elf_power: i32) -> Self {
+    fn from_input(input: &str) -> Self {
         let mut units = vec![];
         let mut map = vec![];
 
@@ -40,7 +43,7 @@ impl Battle {
             for (x, c) in line.char_indices() {
                 xs.push(match c {
                     'G' | 'E' => {
-                        units.push(Unit::new((x as i32, y as i32), c == 'E', elf_power));
+                        units.push(Unit::new((x as i32, y as i32), c == 'E'));
                         false
                     }
                     '.' => true,
@@ -55,15 +58,23 @@ impl Battle {
             units,
             map,
             elf_count,
+            elf_power: 3,
             turns: 0,
         }
+    }
+
+    /// Clones this battle, setting elf_power to the provided value in the clone.
+    fn with_elf_power(&self, elf_power: i32) -> Self {
+        let mut result = self.clone();
+        result.elf_power = elf_power;
+        result
     }
 
     /// Runs the fight to completion, and returns the outcome (as described in the puzzle),
     /// as well as a bool saying whether the elves won without casualties.
     fn run_to_completion(mut self) -> (i32, bool) {
         loop {
-            self.units.sort_unstable_by_key(|u| u.priority(false));
+            self.units.sort_unstable_by_key(|u| key(u.pos, 0));
             for i in 0..self.units.len() {
                 if !self.execute_unit_turn(i) {
                     self.units.retain(|u| u.hp > 0);
@@ -81,14 +92,9 @@ impl Battle {
 }
 
 impl Battle {
-    /// Executes the turn of a single unit.
+    /// Executes the turn of a single unit. Returns whether the battle continues.
     fn execute_unit_turn(&mut self, unit: usize) -> bool {
-        let Unit {
-            pos: (x, y),
-            hp,
-            elf,
-            power,
-        } = self.units[unit];
+        let Unit { pos, hp, elf } = self.units[unit];
 
         // Step 0: Dead units can't act.
         if hp <= 0 {
@@ -105,118 +111,75 @@ impl Battle {
             .units
             .iter()
             .filter(|u| u.hp >= 0 && u.elf != elf)
-            .flat_map(|u| DELTAS.map(|(dx, dy)| (u.pos.0 + dx, u.pos.1 + dy)))
-            .filter(|&(tx, ty)| self.open(tx, ty) || (tx, ty) == (x, y))
+            .flat_map(|u| splat(u.pos))
+            .filter(|&p| self[p] || p == pos)
             .collect();
         if targets.is_empty() {
             return true;
         }
 
-        // Step 3: Pick a direction to move in, and do.
-        if let Some((_, (dx, dy))) = self
-            .fewest_reachable_tiles(unit, &targets)
-            .into_iter()
-            .min_by_key(|((x, y), _)| y * 1000 + x)
-        {
-            self.units[unit].pos = (x + dx, y + dy);
-            self.map[y as usize][x as usize] = true;
-            self.map[(y + dy) as usize][(x + dx) as usize] = false;
+        // Step 3: Search for shortest paths to target squares, and pick the first one in
+        //         reading order; if one was found, move to it.
+        let path = {
+            // Breadth-first search, taking steps in reading order. We must take one step in all
+            // directions at the same time, because we have to find all paths that terminate
+            // at the same length (in order to pick the final one based on other criteria).
+            let mut visited = HashSet::from([pos]);
+            let mut heads = vec![(pos, None)];
+
+            while !heads.is_empty() && !heads.iter().any(|(p, _)| targets.contains(p)) {
+                let mut next = vec![];
+                for (s, dir) in heads {
+                    for p in splat(s) {
+                        if visited.insert(p) && self[p] {
+                            next.push((p, dir.or_else(|| Some(sub(p, s)))));
+                        }
+                    }
+                }
+                heads = next;
+            }
+
+            heads
+                .into_iter()
+                .filter(|(p, _)| targets.contains(p))
+                .min_by_key(|(p, _)| key(*p, 0))
+        };
+        if let Some((_, Some(dir))) = path {
+            self.units[unit].pos = add(pos, dir);
+            self[pos] = true;
+            self[add(pos, dir)] = false;
         }
 
-        // Step 4: Strike an adjacent foe.
-        if let Some(target) = self.get_target(unit) {
-            self.units[target].hp -= power;
-            let Unit {
-                pos: (x, y), hp, ..
-            } = self.units[target];
-            if hp <= 0 {
-                self.map[y as usize][x as usize] = true;
+        // Step 4: Find an adjacent foe, and strike it.
+        let pos = self.units[unit].pos;
+        let target = self
+            .units
+            .iter_mut()
+            .filter(|u| u.hp > 0 && u.elf != elf && splat(pos).contains(&u.pos))
+            .min_by_key(|u| key(u.pos, u.hp));
+        if let Some(target) = target {
+            target.hp -= if elf { self.elf_power } else { 3 };
+            if target.hp <= 0 {
+                let pos = target.pos;
+                self[pos] = true;
             }
         }
 
         true
     }
+}
 
-    /// Checks whether a given tile is free or not.
-    fn open(&self, x: impl TryInto<usize>, y: impl TryInto<usize>) -> bool {
-        if let (Ok(x), Ok(y)) = (x.try_into(), y.try_into()) {
-            self.map[y][x]
-        } else {
-            false
-        }
+impl std::ops::Index<(i32, i32)> for Battle {
+    type Output = bool;
+
+    fn index(&self, (x, y): (i32, i32)) -> &Self::Output {
+        &self.map[y as usize][x as usize]
     }
+}
 
-    /// Returns the target for a unit (adjacent foe with lowest HP), if any.
-    fn get_target(&self, unit: usize) -> Option<usize> {
-        let Unit {
-            pos: (x, y),
-            hp,
-            elf,
-            ..
-        } = self.units[unit];
-        if hp <= 0 {
-            return None;
-        }
-
-        self.units
-            .iter()
-            .enumerate()
-            .filter(|(_, u)| {
-                let (ux, uy) = u.pos;
-                u.hp > 0 && u.elf != elf && DELTAS.contains(&(ux - x, uy - y))
-            })
-            .min_by_key(|(_, u)| u.priority(true))
-            .map(|p| p.0)
-    }
-
-    /// Returns a list of all tiles that are reachable in the fewest number of steps, alongside
-    /// the direction to go in in order to reach it.
-    fn fewest_reachable_tiles(
-        &self,
-        unit: usize,
-        targets: &HashSet<(i32, i32)>,
-    ) -> Vec<((i32, i32), (i32, i32))> {
-        let Unit {
-            pos: (x, y), hp, ..
-        } = self.units[unit];
-
-        if hp <= 0 || targets.contains(&(x, y)) {
-            return vec![];
-        }
-
-        // Breadth-first search, taking steps in reading order. Always perform a full depth step
-        // of the breadth first search, in order to catch cases where there are multiple target
-        // squares reached.
-        let mut visited = HashSet::from([(x, y)]);
-        let mut heads = vec![(x, y, None)];
-
-        while !heads.is_empty() && !heads.iter().any(|&(x, y, _)| targets.contains(&(x, y))) {
-            let mut next = vec![];
-            for (x, y, dir) in heads {
-                for (dx, dy) in DELTAS {
-                    let (x, y) = (x + dx, y + dy);
-
-                    if visited.contains(&(x, y)) {
-                        continue;
-                    }
-
-                    visited.insert((x, y));
-                    if self.open(x, y) {
-                        next.push((x, y, dir.or(Some((dx, dy)))));
-                    }
-                }
-            }
-            heads = next;
-        }
-
-        heads
-            .into_iter()
-            .filter_map(|(x, y, dir)| {
-                targets
-                    .contains(&(x, y))
-                    .then(|| ((x, y), dir.unwrap_or((0, 0))))
-            })
-            .collect()
+impl std::ops::IndexMut<(i32, i32)> for Battle {
+    fn index_mut(&mut self, (x, y): (i32, i32)) -> &mut Self::Output {
+        &mut self.map[y as usize][x as usize]
     }
 }
 
@@ -225,28 +188,32 @@ impl Battle {
 struct Unit {
     pos: (i32, i32),
     hp: i32,
-    power: i32,
     elf: bool,
 }
 
 impl Unit {
-    /// Constructs a new unit at the given location. If the unit is an elf, `elf_power` is
-    /// used for `power`, instead of the default value.
-    fn new(pos: (i32, i32), elf: bool, elf_power: i32) -> Self {
-        Self {
-            pos,
-            hp: 200,
-            power: if elf { elf_power } else { 3 },
-            elf,
-        }
-    }
-
-    /// Sorting priority (lower = earlier). `include_hp` is for sorting when selecting
-    /// a target to strike.
-    fn priority(&self, include_hp: bool) -> i32 {
-        (if include_hp { self.hp * 1000000 } else { 0 }) + self.pos.1 * 1000 + self.pos.0
+    /// Constructs a new unit at the given location.
+    fn new(pos: (i32, i32), elf: bool) -> Self {
+        Self { pos, hp: 200, elf }
     }
 }
 
-/// Four directions, in reading order (up, left, right, down).
-static DELTAS: [(i32, i32); 4] = [(0, -1), (-1, 0), (1, 0), (0, 1)];
+// Creates a sorting key that is equivalent to sorting by, in order, `hp`, `y` and `x`.
+fn key((x, y): (i32, i32), hp: i32) -> i32 {
+    (hp << 16) + (y << 8) + x
+}
+
+// Adds two points.
+fn add((ax, ay): (i32, i32), (bx, by): (i32, i32)) -> (i32, i32) {
+    (ax + bx, ay + by)
+}
+
+// Subtracts two points.
+fn sub((ax, ay): (i32, i32), (bx, by): (i32, i32)) -> (i32, i32) {
+    (ax - bx, ay - by)
+}
+
+// A list of points adjacent to the provided one, in reading order.
+fn splat((x, y): (i32, i32)) -> [(i32, i32); 4] {
+    [(x, y - 1), (x - 1, y), (x + 1, y), (x, y + 1)]
+}

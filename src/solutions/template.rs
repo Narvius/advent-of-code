f9e220fa2828022bbdd1c32948018b5c65234542
Phil <narvius@gmail.com>
2022-11-07 14:01:19 +0100

    template: () return value

--- src/solutions/template.rs
+++ src/solutions/template.rs
@@ -1,7 +1,7 @@
-pub fn one(input: &str) -> crate::Result<&str> {
+pub fn one(input: &str) -> crate::Result<()> {
     Err("unimplemented".into())
 }
 
-pub fn two(input: &str) -> crate::Result<&str> {
+pub fn two(input: &str) -> crate::Result<()> {
     Err("unimplemented".into())
 }

cb76ea88e0dc74d7cc97f0677f52e1b46fdc19f3
Phil <narvius@gmail.com>
2022-11-07 14:14:22 +0100

    template: revert () change

--- src/solutions/template.rs
+++ src/solutions/template.rs
@@ -1,7 +1,7 @@
-pub fn one(input: &str) -> crate::Result<()> {
+pub fn one(input: &str) -> crate::Result<&str> {
     Err("unimplemented".into())
 }
 
-pub fn two(input: &str) -> crate::Result<()> {
+pub fn two(input: &str) -> crate::Result<&str> {
     Err("unimplemented".into())
 }

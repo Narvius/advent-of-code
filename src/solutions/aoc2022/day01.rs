34063da808e7c263a08ef7b14bb02c3370864a81
Phil <narvius@gmail.com>
2022-12-01 10:18:37 +0100

    2022-01 better chunking

--- src/solutions/aoc2022/day01.rs
+++ src/solutions/aoc2022/day01.rs
@@ -1,31 +1,18 @@
 /// Find the total caolories carried by the top elf.
 pub fn one(input: &str) -> crate::Result<i32> {
-    Elves(input.lines()).max().ok_or_else(|| "no result".into())
+    parse(input).max().ok_or_else(|| "no result".into())
 }
 
 /// Find the total calories carried by the top three elves.
 pub fn two(input: &str) -> crate::Result<i32> {
-    let mut elves: Vec<_> = Elves(input.lines()).collect();
+    let mut elves: Vec<i32> = parse(input).collect();
     elves.sort_unstable_by_key(|e| i32::MAX - e);
     Ok(elves[0..3].iter().copied().sum())
 }
 
-/// An iterator over all elves in the input.
-struct Elves<'a>(std::str::Lines<'a>);
-
-impl Iterator for Elves<'_> {
-    type Item = i32;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let mut sum = 0;
-        for line in self.0.by_ref() {
-            if line.is_empty() {
-                return Some(sum);
-            } else {
-                sum += line.parse::<i32>().ok()?;
-            }
-        }
-
-        None
-    }
+/// Parses the puzzle input into a list of the calories carried by each elf.
+fn parse(input: &str) -> impl Iterator<Item = i32> + '_ {
+    input
+        .split("\n\n")
+        .map(|chunk| chunk.lines().filter_map(|n| n.parse::<i32>().ok()).sum())
 }

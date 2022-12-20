bcc4403a06956f96701bf9d9178ba77ff489e8f5
Phil <narvius@gmail.com>
2022-12-20 10:06:07 +0100

    2022-20

---
+++ src/solutions/aoc2022/day20.rs
@@ -0,0 +1,50 @@
+/// Mix the list once and calculate a checksum.
+pub fn one(input: &str) -> crate::Result<i64> {
+    let (mut items, mut order) = parse(input, 1);
+    mix_list(&mut items, &mut order)?;
+    grove_coordinates(&mut items)
+}
+
+/// Pre-multiply numbers in the list, mix them 10 times, and calculate the same checksum.
+pub fn two(input: &str) -> crate::Result<i64> {
+    let (mut items, mut order) = parse(input, 811589153);
+    for _ in 0..10 {
+        mix_list(&mut items, &mut order)?;
+    }
+    grove_coordinates(&mut items)
+}
+
+/// Mixes the list as per the puzzle input. `items` and `order` are mixed in the exact same way,
+/// so that consecutive invocations with the same lists will behave correctly for part 2.
+fn mix_list(items: &mut Vec<i64>, order: &mut Vec<usize>) -> crate::Result<()> {
+    for i in 0..items.len() {
+        let Some(p) = order.iter().position(|&n| i == n) else { Err("failed")? };
+        let item = items.remove(p);
+        let new_pos = (p as i64 + item).rem_euclid(items.len() as i64) as usize;
+        items.insert(new_pos, item);
+        let item = order.remove(p);
+        order.insert(new_pos, item);
+    }
+    Ok(())
+}
+
+/// Calculates the grove coordinates from a list of items.
+fn grove_coordinates(items: &mut [i64]) -> crate::Result<i64> {
+    let Some(zero_pos) = items.iter().position(|&n| n == 0) else { Err("failed")? };
+    items.rotate_left(zero_pos);
+
+    Ok(items[1000 % items.len()] + items[2000 % items.len()] + items[3000 % items.len()])
+}
+
+/// Parses the puzzle input into a list of numbers (with an optional multiplier applied), and
+/// an "order" list, which indicates the order in which numbers should be moved. For details,
+/// see the puzzle description.
+fn parse(input: &str, multiplier: i64) -> (Vec<i64>, Vec<usize>) {
+    let items: Vec<_> = input
+        .lines()
+        .filter_map(|v| v.parse::<i64>().ok())
+        .map(|v| v * multiplier)
+        .collect();
+    let order = Vec::from_iter(0..items.len());
+    (items, order)
+}

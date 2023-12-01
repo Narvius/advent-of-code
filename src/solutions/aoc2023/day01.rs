b0296356bbb2c73a3c6a197bbd7474f8c57a53f3
Phil <narvius@gmail.com>
2023-12-01 21:18:18 +0100

    2023-01 shorten substitutes

--- src/solutions/aoc2023/day01.rs
+++ src/solutions/aoc2023/day01.rs
@@ -24,13 +24,13 @@ fn calibration_score(s: &str) -> Option<i32> {
 ///
 /// If numbers are overlapping, like for example `eightwo`, both are added.
 fn with_text_numbers(line: &str) -> String {
-    line.replace("one", "o1ne")
-        .replace("two", "t2wo")
-        .replace("three", "t3hree")
-        .replace("four", "f4our")
-        .replace("five", "f5ive")
-        .replace("six", "s6ix")
-        .replace("seven", "s7even")
-        .replace("eight", "e8ight")
-        .replace("nine", "n9ine")
+    line.replace("one", "o1e")
+        .replace("two", "t2o")
+        .replace("three", "t3e")
+        .replace("four", "f4r")
+        .replace("five", "f5e")
+        .replace("six", "s6x")
+        .replace("seven", "s7n")
+        .replace("eight", "e8t")
+        .replace("nine", "n9e")
 }

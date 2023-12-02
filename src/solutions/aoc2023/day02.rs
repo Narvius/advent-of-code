25eff780e282dfc600a92952b89592dffb6cdf53
Phil <narvius@gmail.com>
2023-12-02 15:57:31 +0100

    2023-02 `parse` into array instead of tuple

--- src/solutions/aoc2023/day02.rs
+++ src/solutions/aoc2023/day02.rs
@@ -2,31 +2,28 @@
 pub fn one(input: &str) -> crate::Result<usize> {
     Ok(parse(input)
         .enumerate()
-        .filter(|&(_, (r, g, b))| r <= 12 && g <= 13 && b <= 14)
+        .filter(|&(_, [r, g, b])| r <= 12 && g <= 13 && b <= 14)
         .map(|(n, _)| n + 1)
         .sum())
 }
 
 /// For each game, find the minimum number of cubes required.
 pub fn two(input: &str) -> crate::Result<usize> {
-    Ok(parse(input).map(|(r, g, b)| r * g * b).sum())
+    Ok(parse(input).map(|[r, g, b]| r * g * b).sum())
 }
 
 /// Parse the puzzle input in such a way that the order from the input file is preserved (making
-/// returning the Game Id unnecessary), and the returned value is a 3-tuple of the highest amount
+/// returning the Game Id unnecessary), and the returned value is an array of the highest amount
 /// of red, green and blue cubes shown, respectively.
-fn parse(input: &str) -> impl Iterator<Item = (usize, usize, usize)> + '_ {
+fn parse(input: &str) -> impl Iterator<Item = [usize; 3]> + '_ {
     input.lines().filter_map(|line| {
-        let (mut r, mut g, mut b) = (0, 0, 0);
+        let mut colors = [0, 0, 0];
         for entry in line.split(&[':', ';', ','][..]) {
             let (n, color) = entry.trim().split_once(' ')?;
-            match color {
-                "red" => r = r.max(n.parse().ok()?),
-                "green" => g = g.max(n.parse().ok()?),
-                "blue" => b = b.max(n.parse().ok()?),
-                _ => {}
-            };
+            if let Some(p) = ["red", "green", "blue"].iter().position(|&c| c == color) {
+                colors[p] = colors[p].max(n.parse().ok()?);
+            }
         }
-        Some((r, g, b))
+        Some(colors)
     })
 }

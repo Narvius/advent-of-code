8a0cea39f7b071b5cea4ae947bb5ed5394af8cc5
Phil <narvius@gmail.com>
2023-12-04 08:38:47 +0100

    2023-04

---
+++ src/solutions/aoc2023/day04.rs
@@ -0,0 +1,41 @@
+/// Using scoring rules, find the total score of all scratchcards.
+pub fn one(input: &str) -> crate::Result<u32> {
+    Ok(parse(input)
+        .filter(|&c| c > 0)
+        .map(|c| 2u32.pow(c as u32 - 1))
+        .sum())
+}
+
+/// Using recursive scratchcard rules, find the total number of scratchcards gained.
+pub fn two(input: &str) -> crate::Result<usize> {
+    let cards: Vec<_> = parse(input).collect();
+    let mut counts = vec![1; cards.len()];
+
+    for i in 0..cards.len() {
+        for n in 1..=cards[i] {
+            counts[i + n] += counts[i];
+        }
+    }
+
+    Ok(counts.into_iter().sum())
+}
+
+/// For each line of input, returns the "score" (count of winning numbers) for that line.
+fn parse(input: &str) -> impl Iterator<Item = usize> + '_ {
+    input.lines().filter_map(|line| {
+        let (_, numbers) = line.split_once(": ")?;
+        let (cards, draws) = numbers.split_once(" | ")?;
+        let cards: Vec<_> = cards
+            .split_whitespace()
+            .filter_map(|n| n.parse::<i32>().ok())
+            .collect();
+
+        Some(
+            draws
+                .split_whitespace()
+                .filter_map(|n| n.parse().ok())
+                .filter(|n| cards.contains(n))
+                .count(),
+        )
+    })
+}

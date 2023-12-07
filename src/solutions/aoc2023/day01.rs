752cb70801920a547ce056ce39dec0834a653921
Phil <narvius@gmail.com>
2023-12-07 19:25:57 +0100

    2023-01 replace `replace`s to reduce runtime

--- src/solutions/aoc2023/day01.rs
+++ src/solutions/aoc2023/day01.rs
@@ -5,10 +5,7 @@ pub fn one(input: &str) -> crate::Result<i32> {
 
 /// Find the total calibration score of all lines, but including numbers written as text.
 pub fn two(input: &str) -> crate::Result<i32> {
-    Ok(input
-        .lines()
-        .filter_map(|line| calibration_score(&with_text_numbers(line)))
-        .sum())
+    Ok(input.lines().filter_map(calibration_score_with_text).sum())
 }
 
 /// Calculates the calibration score for a string, as per the puzzle description.
@@ -19,18 +16,42 @@ fn calibration_score(s: &str) -> Option<i32> {
     Some((10 * (first - b'0') + (last - b'0')) as i32)
 }
 
-/// For each number from 1 to 9 written in plain text (so `one`, `two`, `three`...), adds a
-/// corresponding numerical digit into the string.
+/// Calculates the calibration score for a string, as per the puzzle description; but also
+/// considers numbers written in plain text while doing so.
 ///
-/// If numbers are overlapping, like for example `eightwo`, both are added.
-fn with_text_numbers(line: &str) -> String {
-    line.replace("one", "o1e")
-        .replace("two", "t2o")
-        .replace("three", "t3e")
-        .replace("four", "f4r")
-        .replace("five", "f5e")
-        .replace("six", "s6x")
-        .replace("seven", "s7n")
-        .replace("eight", "e8t")
-        .replace("nine", "n9e")
+/// Originally, I wrote a version that was less code (consisting of only a bunch of replaces), but
+/// this version if faster, running in less than a millisecond for me (...when I compile as
+/// --release, anyway).
+fn calibration_score_with_text(line: &str) -> Option<i32> {
+    fn scan(s: &str, range: impl Iterator<Item = usize>) -> Option<i32> {
+        for i in range {
+            if s.as_bytes()[i].is_ascii_digit() {
+                return Some((s.as_bytes()[i] - b'0') as i32);
+            } else if s[i..].starts_with("one") {
+                return Some(1);
+            } else if s[i..].starts_with("two") {
+                return Some(2);
+            } else if s[i..].starts_with("three") {
+                return Some(3);
+            } else if s[i..].starts_with("four") {
+                return Some(4);
+            } else if s[i..].starts_with("five") {
+                return Some(5);
+            } else if s[i..].starts_with("six") {
+                return Some(6);
+            } else if s[i..].starts_with("seven") {
+                return Some(7);
+            } else if s[i..].starts_with("eight") {
+                return Some(8);
+            } else if s[i..].starts_with("nine") {
+                return Some(9);
+            }
+        }
+        None
+    }
+
+    let first = scan(line, 0..line.len())?;
+    let last = scan(line, (0..line.len()).rev())?;
+
+    Some(10 * first + last)
 }

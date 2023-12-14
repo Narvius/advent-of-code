8193b2e7d43077b86971ffe31cebe3fb1e05c6ba
Phil <narvius@gmail.com>
2023-12-14 12:06:40 +0100

    2023-14

---
+++ src/solutions/aoc2023/day14.rs
@@ -0,0 +1,121 @@
+use std::collections::HashMap;
+
+/// Find the weight on the northern support beams.
+///
+/// Can be done without actually moving the rocks; after writing part 2 I could technically replace
+/// this with less code by factoring out parsing/score calculation and using that + [`tilt`], but
+/// I'm happy with this solution.
+pub fn one(input: &str) -> crate::Result<usize> {
+    let map: Vec<_> = input.lines().map(str::as_bytes).collect();
+
+    let mut total = 0;
+    for y in 0..map.len() {
+        for x in 0..map[y].len() {
+            if map[y][x] == b'O' {
+                let mut added_weight = 0;
+                for dy in 1..=y {
+                    match map[y - dy][x] {
+                        b'.' => added_weight += 1,
+                        b'#' => break,
+                        _ => {}
+                    }
+                }
+                total += map.len() - y + added_weight;
+            }
+        }
+    }
+    Ok(total)
+}
+
+/// Simulate 1000000000 spin cycles, and find the weight on the northern support beams.
+pub fn two(input: &str) -> crate::Result<i32> {
+    let width = input.lines().next().map(str::len).unwrap_or(0);
+    let height = input.lines().count();
+    let mut seen = HashMap::new();
+
+    let mut map = Map {
+        data: input.bytes().filter(|c| !c.is_ascii_control()).collect(),
+        width: width as i32,
+        height: height as i32,
+    };
+
+    seen.insert(map.data.clone(), 0);
+    for i in 1.. {
+        spin_cycle(&mut map);
+
+        if let Some(v) = seen.insert(map.data.clone(), i) {
+            let left = (1000000000 - i) % (i - v);
+            for _ in 0..left {
+                spin_cycle(&mut map);
+            }
+
+            let items = map.data.iter().enumerate();
+            return Ok(items
+                .map(|(i, t)| match t {
+                    b'O' => map.height - i as i32 / map.width,
+                    _ => 0,
+                })
+                .sum());
+        }
+    }
+
+    Err("unimplemented".into())
+}
+
+struct Map {
+    /// Contains the map data, as a flat array.
+    data: Vec<u8>,
+    width: i32,
+    height: i32,
+}
+
+impl Map {
+    /// Returns the element at the given position. Treats out of bounds positions as unmoveable
+    /// rocks.
+    fn get(&self, x: i32, y: i32) -> u8 {
+        if !((0..self.width).contains(&x) && (0..self.height).contains(&y)) {
+            b'#'
+        } else {
+            self.data[(y * self.width + x) as usize]
+        }
+    }
+
+    /// Sets the given position to `value`. Doesn't do anything for out-of-bounds positions.
+    fn set(&mut self, x: i32, y: i32, value: u8) {
+        if (0..self.width).contains(&x) && (0..self.height).contains(&y) {
+            self.data[(y * self.width + x) as usize] = value;
+        }
+    }
+}
+
+/// Performs a single tilt. `ys` and `xs` are given as parameters because we might have to iterate
+/// from different directions to avoid spurious collisions. For example, when tilting east, we want
+/// to move the right-most rocks first, when tilting west, we want to move the left-most ones
+/// first.
+fn tilt<Y, X>(map: &mut Map, ys: Y, xs: X, (dx, dy): (i32, i32))
+where
+    Y: Iterator<Item = i32>,
+    X: Iterator<Item = i32> + Clone,
+{
+    for y in ys {
+        for x in xs.clone() {
+            if map.get(x, y) == b'O' {
+                map.set(x, y, b'.');
+                for d in 1.. {
+                    if map.get(x + dx * d, y + dy * d) != b'.' {
+                        map.set(x + dx * (d - 1), y + dy * (d - 1), b'O');
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+/// Does one spin cycle, as described in the puzzle input.
+fn spin_cycle(map: &mut Map) {
+    tilt(map, 0..map.height, 0..map.width, (0, -1)); // North
+    tilt(map, 0..map.height, 0..map.width, (-1, 0)); // West
+    tilt(map, (0..map.height).rev(), 0..map.width, (0, 1)); // South
+    tilt(map, 0..map.height, (0..map.width).rev(), (1, 0)); // East
+}

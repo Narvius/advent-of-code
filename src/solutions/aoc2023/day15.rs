75716e6ea9b521b12cb7c934ac138bfdee60504d
Phil <narvius@gmail.com>
2023-12-15 09:49:15 +0100

    2023-15 minor refactors

--- src/solutions/aoc2023/day15.rs
+++ src/solutions/aoc2023/day15.rs
@@ -1,24 +1,21 @@
 /// Find the [HASH](hash) of each input instruction, and sum them all.
 pub fn one(input: &str) -> crate::Result<usize> {
-    Ok(input.split(',').map(hash).sum())
+    Ok(input.trim().split(',').map(hash).sum())
 }
 
 /// Find the lens configuration described the the input and calculate a checksum.
 pub fn two(input: &str) -> crate::Result<usize> {
     let mut boxes: Vec<Vec<Lens>> = vec![vec![]; 256];
 
-    let instructions = input
-        .split(',')
-        .filter_map(|s| s.trim().split_once(&['-', '='][..]));
-
-    for (label, target) in instructions {
-        let index = hash(label);
+    let instructions = input.trim().split(',');
+    for (label, target) in instructions.filter_map(|s| s.split_once(&['-', '='][..])) {
+        let slot = &mut boxes[hash(label)];
         if target.is_empty() {
-            boxes[index].retain(|e| e.0 != label);
-        } else if let Some(lens) = boxes[index].iter_mut().find(|e| e.0 == label) {
+            slot.retain(|e| e.0 != label);
+        } else if let Some(lens) = slot.iter_mut().find(|e| e.0 == label) {
             *lens = (label, target.parse()?);
         } else {
-            boxes[index].push((label, target.parse()?));
+            slot.push((label, target.parse()?));
         }
     }
 
@@ -38,7 +35,5 @@ type Lens<'a> = (&'a str, usize);
 
 /// Calculates the Holiday ASCII String Helper algorithm value for a string.
 fn hash(s: &str) -> usize {
-    s.trim()
-        .bytes()
-        .fold(0, |acc, n| (acc + n as usize) * 17 % 256)
+    s.bytes().fold(0, |acc, n| (acc + n as usize) * 17 % 256)
 }

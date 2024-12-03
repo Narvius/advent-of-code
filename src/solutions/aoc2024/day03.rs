283c54dcbdf8a502e866d625f5a9149affd4d7a7
Phil <narvius@gmail.com>
2024-12-03 09:26:11 +0100

    2024-03 simpler solution (less parsing)

--- src/solutions/aoc2024/day03.rs
+++ src/solutions/aoc2024/day03.rs
@@ -1,59 +1,33 @@
 /// Find the sum of all `mul` instructions.
 pub fn one(input: &str) -> crate::Result<i32> {
-    sum_muls(input, false)
+    sum_muls(input)
 }
 
 /// Find the sum of all enabled `mul` instructions.
 pub fn two(input: &str) -> crate::Result<i32> {
-    sum_muls(input, true)
+    // Find all the stretches of input data during which `mul`s are enabled.
+    let (leading_enabled, offs) = input.split_once("don't()").unwrap();
+    let enabled_parts = offs
+        .split("don't()")
+        .filter_map(|part| Some(part.split_once("do()")?.1));
+
+    // Sum across all those parts.
+    Ok(std::iter::once(leading_enabled)
+        .chain(enabled_parts)
+        .filter_map(|part| sum_muls(part).ok())
+        .sum::<i32>())
 }
 
-/// Sums the results of all `mul` instructions in the input. If `consider_donts` is true, also
-/// considers `do()` and `don't()` instructions, enabling and disabling upcoming `mul`s
-/// respectively.
-fn sum_muls(mut input: &str, consider_donts: bool) -> crate::Result<i32> {
-    let mut sum = 0;
-    let mut enabled = true;
-    while !input.is_empty() {
-        if input.starts_with("do()") {
-            enabled = true;
-            input = &input["do()".len()..];
-        } else if consider_donts && input.starts_with("don't()") {
-            enabled = false;
-            input = &input["don't()".len()..];
-        } else if let (Some((a, b)), rest) = parse_mul(input, enabled) {
-            sum += a * b;
-            input = rest;
-        } else {
-            input = &input[1..];
-        }
-    }
-    Ok(sum)
-}
-
-/// Parse a leading token of the form "mul(`a`, `b`)", where `a` and `b` are integers; returns the
-/// parsed pair (if any), and the unparsed remainder of the input string.
-///
-/// If `enabled` is false, always fails, returning no numbers alongside the full input string. I
-/// would have preferred to write "if enabled && let (...) = parse_mul(input)" at the callsite, but
-/// unfortunately let-matching in conditionals like that isn't in stable Rust as of time of writing.
-fn parse_mul(input: &str, enabled: bool) -> (Option<(i32, i32)>, &str) {
-    if !enabled || !input.starts_with("mul(") {
-        return (None, input);
-    }
-
-    let rest = &input["mul(".len()..];
-    let a_len = rest.chars().take_while(char::is_ascii_digit).count();
-    let (a, rest) = rest.split_at(a_len);
-    if let Some(rest) = rest.strip_prefix(',') {
-        let b_len = rest.chars().take_while(char::is_ascii_digit).count();
-        let (b, rest) = rest.split_at(b_len);
-        if let Some(rest) = rest.strip_prefix(')') {
-            return (
-                Some((a.parse::<i32>().unwrap(), b.parse::<i32>().unwrap())),
-                rest,
-            );
-        }
-    }
-    (None, input)
+/// Sums the results of all valid `mul` instructions within `input`. Does not consider `do()` or
+/// `don't()` instructions at all and simply assumes all contained `mul`s are enabled.
+fn sum_muls(input: &str) -> crate::Result<i32> {
+    Ok(input
+        .split("mul(")
+        .filter_map(|t| {
+            let (t, _) = t.split_once(')')?;
+            let (a, b) = t.split_once(',')?;
+            let (a, b) = (a.parse::<i32>().ok()?, b.parse::<i32>().ok()?);
+            Some(a * b)
+        })
+        .sum())
 }

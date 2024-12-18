3a845431708a8c1a8cfa93fada26216ef1156213
Phil <narvius@gmail.com>
2024-12-18 09:57:37 +0100

    2023-17 use `common::Grid`

--- src/solutions/aoc2023/day17.rs
+++ src/solutions/aoc2023/day17.rs
@@ -1,12 +1,12 @@
-use crate::common::astar;
+use crate::common::{astar, Dir, Grid};
 
 pub fn one(input: &str) -> crate::Result<usize> {
-    let map: Vec<Vec<_>> = input.lines().map(|l| l.as_bytes().to_vec()).collect();
+    let map = Grid::from_input(input);
     astar::shortest_path_length(Crucible::new(false), &map).ok_or("no result".into())
 }
 
 pub fn two(input: &str) -> crate::Result<usize> {
-    let map: Vec<Vec<_>> = input.lines().map(|l| l.as_bytes().to_vec()).collect();
+    let map = Grid::from_input(input);
     astar::shortest_path_length(Crucible::new(true), &map).ok_or("no result".into())
 }
 
@@ -14,7 +14,7 @@ pub fn two(input: &str) -> crate::Result<usize> {
 #[derive(Clone, Eq, Hash, PartialEq)]
 struct Crucible {
     position: (i32, i32),
-    direction: (i32, i32),
+    heading: Dir,
     straights: i32,
     ultra: bool,
 }
@@ -24,7 +24,7 @@ impl Crucible {
     fn new(ultra: bool) -> Self {
         Self {
             position: (0, 0),
-            direction: (1, 0),
+            heading: Dir::E,
             straights: 0,
             ultra,
         }
@@ -32,16 +32,15 @@ impl Crucible {
 
     /// Creates a new [`Crucible`] that is the result of taking a step in the provided
     /// direction.
-    fn in_dir(&self, (dx, dy): (i32, i32)) -> Self {
-        let straights = 1 + if self.direction == (dx, dy) {
-            self.straights
-        } else {
-            0
+    fn in_dir(&self, heading: Dir) -> Self {
+        let straights = 1 + match self.heading == heading {
+            true => self.straights,
+            _ => 0,
         };
-        let (x, y) = self.position;
+
         Self {
-            position: (x + dx, y + dy),
-            direction: (dx, dy),
+            position: self.position + heading,
+            heading,
             straights,
             ultra: self.ultra,
         }
@@ -50,51 +49,41 @@ impl Crucible {
     /// Checks if this [`Crucible`] is valid (hasn't stepped off the map and not taken too many
     /// steps forward), and if so, returns itself alongside the cost of having done the last
     /// step.
-    fn with_cost(self, map: &[Vec<u8>]) -> Option<(Self, usize)> {
-        let (Ok(x), Ok(y)) = (
-            usize::try_from(self.position.0),
-            usize::try_from(self.position.1),
-        ) else {
-            return None;
-        };
-
+    fn with_cost(self, grid: &Grid<u8>) -> Option<(Self, usize)> {
         let straight_cap = if self.ultra { 10 } else { 3 };
         if self.straights > straight_cap {
             return None;
         }
 
-        map.get(y)
-            .and_then(|r| r.get(x))
-            .map(|&cost| (self, (cost - b'0') as usize))
+        grid.get(self.position)
+            .map(|cost| (self, (cost - b'0') as usize))
     }
 }
 
 impl<'a> astar::Node<'a> for Crucible {
     type Cost = usize;
-    type Env = Vec<Vec<u8>>;
+    type Env = Grid<'a, u8>;
 
     fn next(&self, env: &'a Self::Env) -> Box<dyn Iterator<Item = (Self, Self::Cost)> + 'a> {
-        let (dx, dy) = self.direction;
-        let left = (dy, -dx);
-        let right = (-dy, dx);
-
         match self.ultra {
             false => Box::new(
-                [self.in_dir((dx, dy)), self.in_dir(left), self.in_dir(right)]
+                [self.heading, self.heading.left(), self.heading.right()]
+                    .map(|d| self.in_dir(d))
                     .into_iter()
-                    .filter_map(|s| s.with_cost(env)),
+                    .filter_map(|node| node.with_cost(env)),
             ),
             true => {
                 let s = self.straights;
                 Box::new(
                     [
-                        (s < 10).then(|| self.in_dir(self.direction)),
-                        (4 <= s || self.position == (0, 0)).then(|| self.in_dir(left)),
-                        (4 <= s || self.position == (0, 0)).then(|| self.in_dir(right)),
+                        (s < 10).then_some(self.heading),
+                        (4 <= s || self.position == (0, 0)).then(|| self.heading.left()),
+                        (4 <= s || self.position == (0, 0)).then(|| self.heading.right()),
                     ]
+                    .map(|d| d.map(|d| self.in_dir(d)))
                     .into_iter()
                     .flatten()
-                    .filter_map(|s| s.with_cost(env)),
+                    .filter_map(move |node| node.with_cost(env)),
                 )
             }
         }
@@ -102,18 +91,15 @@ impl<'a> astar::Node<'a> for Crucible {
 
     fn heuristic(&self, env: &Self::Env) -> Self::Cost {
         let (x, y) = self.position;
-        let (width, height) = (env[0].len(), env.len());
-
-        (x as usize).abs_diff(width) + (y as usize).abs_diff(height)
+        (x as usize).abs_diff(env.width()) + (y as usize).abs_diff(env.height())
     }
 
     fn done(&self, env: &Self::Env) -> bool {
-        let (x, y) = self.position;
-        let (width, height) = (env[0].len() as i32, env.len() as i32);
+        let (x, y) = (self.position.0 as usize, self.position.1 as usize);
 
-        let position_fits = (x + 1) == width && (y + 1) == height;
+        let reached_end = x == env.width() - 1 && y == env.height() - 1;
         let ultra = !self.ultra || self.straights >= 4;
 
-        position_fits && ultra
+        reached_end && ultra
     }
 }

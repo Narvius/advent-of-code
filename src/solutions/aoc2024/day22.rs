285292f0cc9efd6aba7c9974f1f3eaad3b60eb58
Phil <narvius@gmail.com>
2024-12-23 01:22:17 +0100

    2024-22 comments; replace loops with scan + for
    I'm starting to really enjoy `(0..x).scan(whatever)` as a pattern,
    though I'm not certain it's "better" than `successors` or just a loop.
    
    Also, storing all 2001 secret numbers so I can `windows(5)` over it is
    perfectly fine. I don't need to manually keep track of the last four as
    I consume the iterator.

--- src/solutions/aoc2024/day22.rs
+++ src/solutions/aoc2024/day22.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, mem::replace};
 
 /// Find the sum of the 2000th generated secret number of each buyer.
 pub fn one(input: &str) -> crate::Result<usize> {
@@ -12,37 +12,37 @@ pub fn one(input: &str) -> crate::Result<usize> {
 /// For each buyer, finds all possible sales; and then simply sums up the price for each
 /// negotiation pattern across every buyer, and picks the highest one.
 pub fn two(input: &str) -> crate::Result<usize> {
-    let mut bananas: HashMap<(i32, i32, i32, i32), usize> = HashMap::new();
-    for mut n in parse(input) {
-        #[allow(unused_assignments)]
-        let (mut m4, mut m3, mut m2, mut m1) = (None, None, None, None);
-        let mut patterns = HashMap::new();
-        for _ in 0..2000 {
-            (m4, m3, m2, m1, n) = (m3, m2, m1, Some(n % 10), step(n));
+    parse(input)
+        .fold(HashMap::<_, usize>::new(), |mut earnings, n| {
+            // Collect all prices (secret number % 10).
+            let ns: Vec<_> = (0..=2000)
+                .scan(n, |s, _| Some((replace(s, step(*s)) % 10) as i32))
+                .collect();
 
-            if let (Some(m4), Some(m3), Some(m2), Some(m1)) = (m4, m3, m2, m1) {
-                let key = (
-                    m3 as i32 - m4 as i32,
-                    m2 as i32 - m3 as i32,
-                    m1 as i32 - m2 as i32,
-                    n as i32 % 10 - m1 as i32,
-                );
-
-                patterns.entry(key).or_insert(n % 10);
+            // The first time a pattern of 4 consecutive differences shows up, mark the price.
+            let mut first_prices = HashMap::new();
+            for w in ns.windows(5) {
+                let pattern = (w[1] - w[0], w[2] - w[1], w[3] - w[2], w[4] - w[3]);
+                first_prices.entry(pattern).or_insert(w[4] as usize);
             }
-        }
-        for (key, count) in patterns {
-            *bananas.entry(key).or_default() += count;
-        }
-    }
-    bananas.into_values().max().ok_or("no result".into())
+
+            // Add the price for every pattern into the main earnings map.
+            for (pattern, price) in first_prices {
+                *earnings.entry(pattern).or_default() += price;
+            }
+
+            earnings
+        })
+        .into_values()
+        .max()
+        .ok_or("no result".into())
 }
 
 /// Finds the next secret number, based on the previous one. While it's probably not faster to
 /// transform all of the operations (multiplication, division, modulo) into bit-based operations, I
 /// thought it was funny that you could. So I did.
 fn step(n: usize) -> usize {
-    const PRUNE: usize = usize::MAX ^ (usize::MAX << 24);
+    const PRUNE: usize = !(usize::MAX << 24);
 
     let n = ((n << 6) ^ n) & PRUNE;
     let n = ((n >> 5) ^ n) & PRUNE;

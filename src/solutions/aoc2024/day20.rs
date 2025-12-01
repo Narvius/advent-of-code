76114adf49d3e92cc0890c49ab76f8e33e289455
Phil <narvius@gmail.com>
2025-12-01 08:57:58 +0100

    2024-20 refactor

--- src/solutions/aoc2024/day20.rs
+++ src/solutions/aoc2024/day20.rs
@@ -1,6 +1,4 @@
-use std::collections::HashMap;
-
-use crate::common::{diamond_deltas, Grid, CARDINAL};
+use crate::common::{Grid, CARDINAL};
 
 /// Count the amount of distinct range-2 cheats that save at least 100 steps.
 pub fn one(input: &str) -> crate::Result<usize> {
@@ -14,20 +12,19 @@ pub fn two(input: &str) -> crate::Result<usize> {
 
 /// Counts the amount of distinct cheats that save at least 100 steps. A cheat is a pair of
 /// starting and ending position, where their taxicab distance is less or equal `cheat_duration`.
-fn count_cheats(input: &str, cheat_duration: usize) -> usize {
+fn count_cheats(input: &str, cheat_duration: i32) -> usize {
     let main_path = parse(input);
-    let lookup: HashMap<V2, usize> = (main_path.iter().enumerate())
-        .map(|(i, &p)| (p, i))
-        .collect();
 
-    (main_path.iter().enumerate())
-        .flat_map(|(step, &p)| diamond_deltas(cheat_duration).map(move |d| (step, p, d)))
-        .filter_map(|(step, p, d)| {
-            let cheated_steps = *lookup.get(&(p.0 + d.0, p.1 + d.1))?;
-            let extra_steps = (d.0.abs() + d.1.abs()) as usize;
-            (cheated_steps >= step + extra_steps + 100).then_some(1)
-        })
-        .sum()
+    let mut count = 0;
+    for (i, &(x1, y1)) in main_path.iter().enumerate() {
+        for (normal_steps, &(x2, y2)) in main_path[i..].iter().skip(100).enumerate() {
+            let distance = (x1.abs_diff(x2) + y1.abs_diff(y2)) as i32;
+            if distance <= cheat_duration && (normal_steps as i32 - distance) >= 0 {
+                count += 1;
+            }
+        }
+    }
+    count
 }
 
 type V2 = (i32, i32);
